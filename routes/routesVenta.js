58de49e3494c485600ad5ea3032786a5a89724be
Pablo Cesar Altuzar Grajales <pablocesaraltuzar04@gmail.com>
2023-07-21 13:54:14 -0600

    nuevas cosas

--- routes/routesVenta.js
+++ routes/routesVenta.js
@@ -48,55 +48,62 @@ routesVenta.get("/Productos/:year/:month", (req, res) => {
   });
 });
 
-routesVenta.post("/", async (req, res) => {
-  try {
-    const conn = await req.getConnection();
+routesVenta.post("/", (req, res) => {
+  req.getConnection((err, conn) => {
+    if (err) return res.send(err);
 
-    const ventaData = req.body;
-    const idProducto = ventaData.id_Producto;
+    const ventaData = req.body; // Datos de la venta proporcionados en el cuerpo de la solicitud
+    const idProducto = ventaData.id_Producto; // ID_Producto vendido
+    const cantidadVendida = ventaData.Cantidad_Vendida; // Cantidad vendida en la venta
 
-    const result = await conn.query(
-      "SELECT id_Producto, Cantidad_Disponible FROM Producto WHERE id_Producto = ?",
-      [idProducto]
-    );
+    // Verificar si el producto existe en la tabla Producto
+    conn.query(
+      "SELECT * FROM Producto WHERE id_Producto = ?",
+      [idProducto],
+      (err, result) => {
+        if (err) return res.send(err);
 
-    if (result.length === 0) {
-      return res.status(404).json({ error: "Producto no encontrado" });
-    }
+        if (result.length === 0) {
+          // El producto no existe en la tabla Producto
+          return res.status(404).json({ error: "Producto no encontrado" });
+        }
 
-    const cantidadVendida = ventaData.Cantidad_Vendida;
-    const cantidadDisponible = result[0].Cantidad_Disponible;
+        const producto = result[0];
+        const cantidadDisponible = producto.Cantidad_Disponible;
 
-    if (cantidadDisponible < cantidadVendida) {
-      return res.status(400).json({ error: "No hay suficiente cantidad disponible para vender" });
-    }
+        if (cantidadVendida > cantidadDisponible) {
+          // La cantidad vendida supera la cantidad disponible
+          return -1;
+        }
 
-    const nuevaCantidadDisponible = cantidadDisponible - cantidadVendida;
+        // Realizar la inserción en la tabla Venta
+        conn.query("INSERT INTO Venta SET ?", [ventaData], (err, result) => {
+          if (err) return res.send(err);
 
-    await conn.beginTransaction();
+          // Actualizar la cantidad disponible en la tabla Producto
+          const nuevaCantidadDisponible = cantidadDisponible - cantidadVendida;
+          let nuevoEstado = producto.Estado;
 
-    try {
-      await conn.query("UPDATE Producto SET Cantidad_Disponible = ? WHERE id_Producto = ?", [
-        nuevaCantidadDisponible,
-        idProducto,
-      ]);
+          if (nuevaCantidadDisponible <= 0) {
+            nuevoEstado = "Inactivo";
+          }
 
-      await conn.query("INSERT INTO Venta SET ?", [ventaData]);
+          conn.query(
+            "UPDATE Producto SET Cantidad_Disponible = ?, Estado = ? WHERE id_Producto = ?",
+            [nuevaCantidadDisponible, nuevoEstado, idProducto],
+            (err, updateResult) => {
+              if (err) return res.send(err);
 
-      if (nuevaCantidadDisponible <= 0) {
-        await conn.query("DELETE FROM Producto WHERE id_Producto = ?", [idProducto]);
+              res.json(updateResult);
+            }
+          );
+        });
       }
-
-      await conn.commit();
-      res.json({ message: "Venta realizada y producto eliminado si Cantidad_Disponible <= 0" });
-    } catch (err) {
-      await conn.rollback();
-      throw err;
-    }
-  } catch (err) {
-    res.send(err);
-  }
+    );
+  });
 });
+
+
 
 
 
